8a3ac4a0178cf5e7632836cbb483d6e91ade762c
Alex Zhang <zhangchi0104@live.com>
2024-01-15 19:30:15 +1100

    updated autohost port

--- config.ts
+++ config.ts
@@ -1,7 +1,9 @@
+/** @format */
+
 export const plasmidServer = {
-    host: process.env.PLASMID_SERVER_HOST ?? '127.0.0.1',
-    port: Number.parseInt(process.env.PLASMID_SERVER_PORT ?? '5000'),
-}
+  host: process.env.PLASMID_SERVER_HOST ?? '127.0.0.1',
+  port: Number.parseInt(process.env.PLASMID_SERVER_PORT ?? '8081'),
+};
 
 export const dntp = process.env.DNTP_ADDRESS ?? 'http://144.126.145.172:3000';
 export const mapDir = process.env.ENGINE_MAP_DIR ?? 'engine/maps';
